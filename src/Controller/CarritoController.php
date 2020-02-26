3428cb5701f85e038f3ebb18705f3d209f8698e8
Julian <julytoccr@gmail.com>
2020-02-26 12:13:36 -0300

    lista de carrito ok faltan funciones de los links

--- src/Controller/CarritoController.php
+++ src/Controller/CarritoController.php
@@ -3,22 +3,67 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Categorias;
+use App\Entity\Productos;
 
 class CarritoController extends AbstractController
 {
     public function index(){
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/CarritoController.php',
+
+        //Tomo todas la categorias para pasarsela a la vista(menu)
+        $categorias = $this->getDoctrine()->getRepository(Categorias::class)->findAll();
+
+        $carrito = $this->get('session')->get('carrito');
+
+        //renderizo la vista y devuelvo
+        return $this->render('lista_carrito.html.twig',[
+            'categorias'=>$categorias,
+            'carrito'=>$carrito
         ]);
+
     }
 
-    public function add(){
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/CarritoController.php',
-        ]);
+    public function add($id){
+        $producto_id = $id;
+
+        $session = new Session();
+
+        //Existre el carrito?
+        if($session->get('carrito')){
+            $carrito=$session->get('carrito');
+            $counter = 0;
+            foreach($carrito as $indice => $elemento){
+                if($elemento['id_producto'] == $producto_id){
+                    $carrito[$indice]['unidades']++;
+                    $counter++;
+                }
+            }
+        }
+        else{
+            $carrito=array();
+        }
+        if(!isset($counter) || $counter == 0){
+            // Conseguir datos producto a agregar a carrito
+            $producto = $this->getDoctrine()->getRepository(Productos::class)
+                ->find($id);
+
+            // Añadir al carrito
+            if(is_object($producto)){
+                $producto_a_agregar=array(
+                    'id_producto' => $producto->getId(),
+                    'precio' => $producto->getPrecio(),
+                    'unidades' => 1,
+                    'imagen' =>$producto->getImagen(),
+                    'id' =>$producto->getId(),
+                    'nombre' =>$producto->getNombre()
+                );
+                $carrito[]=$producto_a_agregar;
+            }
+        }
+        $session->set('carrito', $carrito);
+        return $this->redirectToRoute('carritoindex');
     }
 
     public function delete(){
